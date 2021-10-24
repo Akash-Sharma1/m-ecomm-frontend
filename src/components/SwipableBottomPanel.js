b0d7f246fd983c97846dcc678a480ad17ca5cda7
Akash Sharma <aakashsharmajc100@gmail.com>
2021-10-24 19:55:35 +0530

    bottom panel swipable via transform

--- src/components/SwipableBottomPanel.js
+++ src/components/SwipableBottomPanel.js
@@ -1,6 +1,6 @@
-import React, from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { PanGestureHandler, ScrollView } from 'react-native-gesture-handler';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 import Animated, {
   useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring,
 } from 'react-native-reanimated';
@@ -9,34 +9,44 @@ import { Colors, Sizes } from 'styles';
 
 const DEFAULT_SIZE = 300;
 const CLOSING_THRESHOLD = 200;
+const CLOSING_HEIGHT = 200;
 
 const SwipableBottomPanel = ({
   children,
   initialHeight = DEFAULT_SIZE,
   closingThreshold = CLOSING_THRESHOLD,
-  closedSize = 50,
+  closedHeight = CLOSING_HEIGHT,
 }) => {
-  const panelHeight = useSharedValue(initialHeight);
+  const panelHeight = useSharedValue(Sizes.SCREEN_HEIGHT - initialHeight);
+  const closedHeightFromTop = Sizes.SCREEN_HEIGHT - closedHeight;
+  const closingThresholdFromTop = Sizes.SCREEN_HEIGHT - closingThreshold;
 
   const gestureHandler = useAnimatedGestureHandler({
     onStart: (_, ctx) => {
       ctx.translationY = panelHeight.value;
     },
     onActive: (event, ctx) => {
-      panelHeight.value = ctx.translationY - event.translationY;
-    },
-    onEnd: () => {
-      const height = panelHeight.value;
-
-      if (height < closingThreshold) {
-        panelHeight.value = withSpring(closedSize);
+      const heightFromTop = ctx.translationY + event.translationY;
+      if (heightFromTop >= 0) {
+        panelHeight.value = heightFromTop;
       }
     },
-  }, [closingThreshold, closedSize]);
+    onEnd: () => {
+      const heightFromTop = panelHeight.value;
+
+      if (heightFromTop >= closingThresholdFromTop) {
+        panelHeight.value = withSpring(closedHeightFromTop);
+      }
+    },
+  }, [closingThresholdFromTop, closedHeightFromTop]);
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      height: panelHeight.value,
+      transform: [
+        {
+          translateY: panelHeight.value,
+        },
+      ],
     };
   });
 
@@ -47,9 +57,9 @@ const SwipableBottomPanel = ({
           <View style={styles.touchBar} />
         </Animated.View>
       </PanGestureHandler>
-      <ScrollView style={styles.scrollView}>
+      <View style={styles.scrollView}>
         {children}
-      </ScrollView>
+      </View>
     </Animated.View>
   );
 };
@@ -61,12 +71,13 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: Sizes.RADIUS_2,
     borderTopRightRadius: Sizes.RADIUS_2,
     backgroundColor: Colors.WHITE,
+    height: Sizes.SCREEN_HEIGHT,
   },
   touchBar: {
     backgroundColor: Colors.GRAY_LIGHT,
     borderRadius: Sizes.RADIUS_2,
     height: Sizes.size(10),
-    width: '40%',
+    width: '20%',
   },
   touchBarContainer: {
     height: Sizes.size(30),
